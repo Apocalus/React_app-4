7cd63b4291065ba87e552180cb6df4a30461d427
Rabeea <59884290+Apocalus@users.noreply.github.com>
2020-06-13 17:17:58 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  let [isLit, setLit] = useState(true);
+  let [isTemp, setTemp] = useState(23)
+
+  function on_update(){
+    console.log("On-Button clicked");
+    setLit(isLit = true)
+  }
+
+  function off_update(){
+    console.log("Off-Button clicked");
+    setLit(isLit = false);
+  }
+
+  function plus_temp(){
+    console.log("Increase-Button clicked")
+    setTemp(++isTemp)
+  }
+
+  function minus_temp(){
+    console.log("Decrease-Button clicked")
+    setTemp(--isTemp)
+  }
+
+
+  return(
+    <div className = {`room ${isLit? "lit" : "dark"}`}>
+      <span id = "lights">
+      The room is {isLit? "lit":"dark"}.
+
+      <div id = "buttons">
+
+      <button id = "on_button" onClick = {on_update}>
+      ON 
+      </button>
+
+      <span id = "off_button">
+
+      <button onClick = {off_update}>
+      OFF
+      </button>
+      </span>
+      </div>
+      </span>
+
+      <div className= "temp">
+        Today's temperature is {isTemp} degrees Celsius.
+      </div>
+      <div id = "buttons">
+      <button onClick = {plus_temp}>
+        Increase
+      </button>
+      <span id = "minus">
+      <button onClick = {minus_temp}>
+        Decrease
+      </button>
+      </span>
+      </div>
+
     </div>
-  );
+      
+    
+  )
+
+
+
+
 }
 
 export default App;
